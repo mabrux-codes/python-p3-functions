1b3c6b837a14b0b28b3e71515b3594f779f30c3b
Abel Lukwaro <abelmabruke@gmail.com>
2025-05-06 13:37:38 +0300

    third python commit

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
-#!/usr/bin/env python3
+# lib/functions.py
 
+# 1. Function that takes no arguments and prints a greeting
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
+# 2. Function that takes one argument and prints a personalized greeting
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
+# 3. Function that takes an optional argument and prints a greeting
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
+# 4. Function that returns the sum of two numbers
 def add(num1, num2):
-    pass
+    return num1 + num2
 
+# 5. Function that returns half the value of a number
 def halve(number):
-    pass
+    return number / 2
